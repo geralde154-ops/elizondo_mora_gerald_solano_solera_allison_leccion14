db4a1a929d86db129efa1b8b606549eb622d107a
gelizondo <geralde154@gmail.com>
2026-08-23 01:15:40 +0000

    Correción de error a la hora de ejecutar todo el proyecto junto con las branch - cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Herramientas from './pages/Herramientas';
 import Menu from './components/Menu';
 
 
